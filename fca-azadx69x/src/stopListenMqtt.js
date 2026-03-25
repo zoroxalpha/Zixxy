dd79fcf19d6ea132086cfec25c895195f0571f95
Azadx69x <nezukojerry803@gmail.com>
2026-03-25 15:13:08 +0600

    X69X BOT V3 Update ✅

---
+++ fca-azadx69x/src/stopListenMqtt.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/*
+@Azadx69x
+*/
+
+module.exports = function (defaultFuncs, api, ctx){
+  return function stopListenMqtt() {
+    if (!ctx.mqttClient) {
+      throw new Error("Not connected to MQTT");
+    }
+    utils.log("stopListenMqtt", "Stopping...");
+    ctx.mqttClient.unsubscribe("/webrtc");
+    ctx.mqttClient.unsubscribe("/rtc_multi");
+    ctx.mqttClient.unsubscribe("/onevc");
+    ctx.mqttClient.publish("/browser_close", "{}");
+    ctx.mqttClient.end(false, (...data) => {
+      utils.log("stopListenMqtt", "Stopped");
+      ctx.mqttClient = null;
+    });
+  }
+};
